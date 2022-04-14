5afe9d07f13094f633486fafd8f4e01d2a2efe33
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-14 11:06:41 +0600

    app.js, weather.js: moved state and useeffect from weather.js to app.js; devided useeffect into two.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,74 @@
-import { useState, useMemo } from "react";
-
+import { useState, useMemo, useEffect } from "react";
 import styles from './app.module.css';
-
+import APIkey from '../../APIkey';
 import CityForm from "../City-form";
 import Weather from "../Weather";
 
 function App() {
   const [city, setCity] = useState('');
-  
+  const [dataCoordinates, setDataCoordinates] = useState(null);
+  const [dataWeather, setDataWeather] = useState(null);
+  const [error, setError] = useState(null);
+
   // менет переменную city в зависимости, что передано с CityForm при событии submit
   const onChange = (value) => {
     setCity(value);
   }
+  
+  // идет на API после return 'Enter city...', не доходя до основного render
+  useEffect(() => {
+    // функция запроса на API вызывается в useEffect после первого рендера 
+    const getDataCoordinatesAPI = async (APIkey) => {
+      try {
+        // делает запрос, чтобы получить координаты
+        const res = await fetch(
+          `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIkey}`
+        );
+        const data = await res.json();
 
+        //усли статус не ok выбросить ошибку и перейти в блок catch
+        if (!res.ok) throw data;
+
+        setDataCoordinates(data); // устанавливает новое состояние города
+        setError(null); // обнуляем ошибку при успешном запросе
+      } catch(error) {
+        setError(error); // устанавливаем состояние ошибки
+      }
+    };
+
+    // если city не пустое делаем запрос на API
+    if (city) {
+      getDataCoordinatesAPI(APIkey);
+    }
+  }, [city]);
+  
+  useEffect(() => {
+    // функция запроса на API вызывается в useEffect после первого рендера 
+    const getdataWeatherAPI = async (APIkey) => {
+      try {
+        const lat = dataCoordinates.map((item) => item.lat);
+        const lon = dataCoordinates.map((item) => item.lon);       
+        // делает запрос по координатам, чтобы получить данные погоды
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`
+        );
+        const data = await res.json();
+
+        if(!res.ok) throw data;
+
+        setDataWeather(data);  // устанавливает новое состояние
+        setError(null); // обнуляем ошибку при успешном запросе
+      } catch(error) {
+        setError(error); // устанавливаем состояние ошибки
+      }
+    };
+
+    // если city не пустое делаем запрос на API
+    if (dataCoordinates) {
+      getdataWeatherAPI(APIkey);
+    }
+  }, [dataCoordinates]);
+  
   const iso = useMemo(
     () => require('iso-3166-1'),
     []
@@ -28,7 +84,11 @@ function App() {
   return (
     <div className={styles.app}>
       <CityForm onChange={onChange}/>
-      <Weather city={city} countries={countries}/>
+      <Weather
+      countries={countries}
+      dataCoordinates={dataCoordinates}
+      dataWeather={dataWeather}
+      error={error}/>
     </div>
   );
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,58 +1,9 @@
-import {useState, useEffect, useMemo} from 'react';
 import PropTypes from 'prop-types';
-
 import styles from './weather.module.css';
-
-import APIkey from '../../APIkey';
 import Error from '../Error';
 
-function Weather({city, countries}) {
-  // устанавлииваем первоначальное состоние
-  const [cityName, setCityName] = useState(null);
-  const [dataWeather, setDataWeather] = useState(null);
-  const [error, setError] = useState(null); 
-  
-  // идет на API после return 'Enter city...', не доходя до основного render
-  useEffect(() => {
-    // функция запроса на API вызывается в useEffect после первого рендера 
-    const getWeatherFromAPI = async (APIkey) => {
-      try {
-        // делает запрос, чтобы получить координаты
-        const resCoordinates = await fetch(
-          `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIkey}`
-        );
-        const dataCoordinates = await resCoordinates.json();
-
-        //усли статус не ok выбросить ошибку и перейти в блок catch
-        if (!resCoordinates.ok) throw dataCoordinates;
-
-        const cityName = dataCoordinates.map((item) => item.name);
-        const [lat] = dataCoordinates.map((item) => item.lat);
-        const [lon] = dataCoordinates.map((item) => item.lon);
-
-        setCityName(cityName); // устанавливает новое состояние города
-        
-        // делает запрос по координатам, чтобы получить данные погоды
-        const resWeather = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`
-        );
-        const data = await resWeather.json();
-
-        if(!resWeather.ok) throw data;
-
-        setDataWeather(data);  // устанавливает новое состояние
-        setError(null); // обнуляем ошибку при успешном запросе
-      } catch(error) {
-        setError(error); // устанавливаем состояние ошибки
-      }
-    };
-
-    // если city не пустое делаем запрос на API
-    if (city) {
-      getWeatherFromAPI(APIkey);
-    }
-  }, [city]);
-
+function Weather({countries, dataCoordinates, dataWeather, error}) {
+  console.log(countries);
   // если состояние не установлено и нет ошибки возвразает Enter city please и дальше не идет
   if (dataWeather === null && !error) {
     return <h3 className={styles.weather}>Enter city please</h3>;
@@ -73,7 +24,7 @@ function Weather({city, countries}) {
   const degreeСelsius = (main.temp - 273.15).toFixed(1);
   return (
     <div className={styles.weather}>
-      <h2 className={styles.title}>{`City: ${cityName}`}</h2>
+      <h2 className={styles.title}>{`City: ${dataCoordinates.map((item) => item.name)}`}</h2>
       <h2 className={styles.title}>{`Temperature:  ${degreeСelsius}ºC`}</h2>
       <h2 className={styles.title}>{`Country:  ${country}`}</h2>
     </div>
