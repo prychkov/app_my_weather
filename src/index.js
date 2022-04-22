ab81f8a7086b8f53bfa28962695eb975d6c62715
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-22 11:01:26 +0600

    index.js: added import store.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
+import store from './redux/store';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
