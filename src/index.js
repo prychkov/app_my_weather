16a2f464d288da18275534abe55324373dce438d
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-22 11:13:23 +0600

    src/index.js: added provider.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import './index.css';
 
@@ -7,7 +8,9 @@ import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
