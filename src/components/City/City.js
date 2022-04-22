ec777a6812e868c5b12fff149cb73d8b1cecb1bc
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-22 14:15:45 +0600

    city.js: added mapstatetoprops, coordinatesloadingselector, coordinatesloadedselector, coordinateserrorselector.

--- src/components/City/City.js
+++ src/components/City/City.js
@@ -1,15 +1,17 @@
 import useFetch from "../../hooks/use-fetch-coordinates";
+import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import styles from './city.module.css';
 import APIkey from '../../APIkey';
 import Error from '../Error';
 import Weather from "../Weather";
+import {coordinatesLoadingSelector, coordinatesLoadedSelector, coordinatesErrorSelector} from '../../redux/selectors';
 
-function City({city}) {
+function City({city, loading, loaded, error}) {
 
     const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIkey}`;
 
-    const {loading, loaded, coordinates, error} = useFetch(url, city);
+    //const {loading, loaded, coordinates, error} = useFetch(url, city);
 
     if (!loading && !loaded && !error) {
         return <h3 className={styles.city}>Enter city please</h3>;
@@ -25,14 +27,21 @@ function City({city}) {
 
     return (
         <div className={styles.city}>
-            <h2 className={styles.title}>{`City: ${coordinates.map((item) => item.name)}`}</h2>
-            <Weather coordinates={coordinates} />
+            {/* <h2 className={styles.title}>{`City: ${coordinates.map((item) => item.name)}`}</h2>
+            <Weather coordinates={coordinates} /> */}
         </div>
     );
 }
 
 City.propTypes = {
-    city: PropTypes.string.isRequired,
+	city: PropTypes.string.isRequired,
 }
 
-export default City;
+const mapStateToProps = (state) => ({
+	loading: coordinatesLoadingSelector(state),
+	loaded: coordinatesLoadedSelector(state),
+	error: coordinatesErrorSelector(state),
+});
+
+
+export default connect(mapStateToProps)(City);
