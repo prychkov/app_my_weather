00845b4f133a2bf04fa9deea42f51769d9af3fa8
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-23 12:39:34 +0600

    selectors.js: added weatherselector, weatherloadingselector, weatherloadedselector, weathererrorselector;

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -5,3 +5,9 @@ export const coordinatesSelector = (state) => state.coordinates.data;
 export const coordinatesLoadingSelector = (state) => state.coordinates.loading;
 export const coordinatesLoadedSelector = (state) => state.coordinates.loaded;
 export const coordinatesErrorSelector = (state) => state.coordinates.error;
+
+export const weatherSelector = (state) => state.weather.data;
+
+export const weatherLoadingSelector = (state) => state.weather.loading;
+export const weatherLoadedSelector = (state) => state.weather.loaded;
+export const weatherErrorSelector = (state) => state.weather.error;
