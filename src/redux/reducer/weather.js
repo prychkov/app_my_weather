e6e0f18f3791f9178136fb3f4d457fd9d7df88e3
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-23 12:32:53 +0600

    reducer/weather.js: added import and function.

---
+++ src/redux/reducer/weather.js
@@ -0,0 +1,39 @@
+import {LOAD_WEATHER, REQUEST, SUCCESS, FAILURE} from '../constants';
+
+const initialState = {
+	loading: false,
+	loaded: false,
+	data: null,
+	error: null,
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+	const {type, data, error} = action;
+
+	switch(type) {
+		case LOAD_WEATHER + REQUEST:
+			return {
+				...state,
+				loading: true,
+				error: null,
+			};
+		case LOAD_WEATHER + SUCCESS:
+			return {
+				...state,
+				data,
+				loading: false,
+				loaded: true,
+				error: null,
+			};
+		case LOAD_WEATHER + FAILURE:
+			return {
+				...state,
+				loading: false,
+				loaded: false,
+				error,
+			};
+		default:
+			return state;
+	}
+}
