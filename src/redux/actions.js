ee071a6235dd034b5644b3f4f717c69f74c2b39e
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-26 15:37:59 +0600

    action.js, store.js: action.js: added import REQUEST, SUCCESS, FAILURE, and changed loadCoordinates, loadWeather - added redux-thunk api and deleted unneccessary fields; store.js: added import thunk, thunk in applyMiddleware;

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,21 +1,35 @@
-import {LOAD_COORDINATES, LOAD_WEATHER} from './constants';
+import {LOAD_COORDINATES, LOAD_WEATHER, REQUEST, SUCCESS, FAILURE} from './constants';
 import APIkey from '../APIkey';
 
-export const loadCoordinates = (city) => ({
-	type: LOAD_COORDINATES,
-	CallAPI: `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIkey}`,
-	city,
-});
+export const loadCoordinates = (city) => async (dispatch) => {
+	dispatch({type: LOAD_COORDINATES + REQUEST, city });
 
-export const loadWeather = (coordinates) => {
+	try {
+		const res = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIkey}`);
+		const data = await res.json();             
+
+		if (!res.ok || data.length === 0) throw data;
+
+		dispatch({type: LOAD_COORDINATES + SUCCESS, city, data}); 
+	} catch(error) {
+		dispatch({type: LOAD_COORDINATES + FAILURE, city, error});	
+	}
+};
+
+export const loadWeather = (coordinates) => async (dispatch) => {
 	const lat = coordinates.map((item) => item.lat);
 	const lon = coordinates.map((item) => item.lon);
-	return {
-	type: LOAD_WEATHER,
-	CallAPI: `
-	https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric
-	`,
-	lat,
-	lon,
-	};
+
+	dispatch({type: LOAD_WEATHER + REQUEST, coordinates});
+
+	try {
+		const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=metric`);
+		const data = await res.json();             
+
+		if (!res.ok) throw data;
+
+		dispatch({type: LOAD_WEATHER + SUCCESS, coordinates, data}); 
+	} catch(error) {
+		dispatch({type: LOAD_WEATHER + FAILURE, coordinates, error});	
+	}
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import {applyMiddleware, createStore} from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import api from './middleware/api';
 import reducer from './reducer';
 
 const store = createStore(
 	reducer,
-	composeWithDevTools(applyMiddleware(api)),
+	composeWithDevTools(applyMiddleware(thunk, api)),
 );
 
 export default store;
