afa71498cab972d9f21ba27c4b6ab18e138d3033
Pavel Rychkov <rychkovpavel@yandex.kz>
2022-04-26 14:49:27 +0600

    reducer/coordinates.js: added immer.

--- src/redux/reducer/coordinates.js
+++ src/redux/reducer/coordinates.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import {LOAD_COORDINATES, REQUEST, SUCCESS, FAILURE} from '../constants';
 
 const initialState = {
@@ -8,32 +9,29 @@ const initialState = {
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = initialState, action) => {
-	const {type, data, error} = action;
+export default (state = initialState, action) => produce(state, (draft) => {
+		const {type, data, error} = action;
 
-	switch(type) {
-		case LOAD_COORDINATES + REQUEST:
-			return {
-				...state,
-				loading: true,
-				error: null,
-			};
-		case LOAD_COORDINATES + SUCCESS:
-			return {
-				...state,
-				data,
-				loading: false,
-				loaded: true,
-				error: null,
-			};
-		case LOAD_COORDINATES + FAILURE:
-			return {
-				...state,
-				loading: false,
-				loaded: false,
-				error,
-			};
-		default: 
-			return state;
-	}
-};
+		switch(type) {
+			case LOAD_COORDINATES + REQUEST: {
+				draft.loading = true;
+				draft.error = null;
+				break;
+			}
+			case LOAD_COORDINATES + SUCCESS: {
+				draft.data = data;
+				draft.loading = false;
+				draft.loaded = true;
+				draft.error = null;
+				break;
+			}
+			case LOAD_COORDINATES + FAILURE: {
+				draft.loading = false;
+				draft.loaded = false;
+				draft.error = error;
+				break;
+			}
+			default: 
+				return;
+		}
+	});
